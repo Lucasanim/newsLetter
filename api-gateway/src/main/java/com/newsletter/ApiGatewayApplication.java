028710b8b2fa1363d0497f8bb9db95d982afa098
lucas <lucasanim12@gmail.com>
2022-12-29 18:48:34 -0300

    Implement api-gateway

--- api-gateway/src/main/java/com/newsletter/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/newsletter/ApiGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class UserServiceApplication {
+public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
--- user-service/src/main/java/com/newsletter/Main.java
+++
@@ -1,7 +0,0 @@
-package com.newsletter;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
